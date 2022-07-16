54dd0c768191820859865d444acff273f8691be4
kuma63 <t09min25.y@gmail.com>
2022-07-16 08:01:33 +0000

    [fix]map

--- app/assets/javascripts/map.js
+++
@@ -1,6 +0,0 @@
-function initMap(){
-    let map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: <%= @photo.latitude %>, lng: <%= @photo.longitude %> },
-    zoom: 15
-    });
-}
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
  before_action :authenticate_user!, except: [:top]
  before_action :configure_permitted_parameters, if: :devise_controller?
 
- GOOGLE_API_KEY = ENV["API_KEY"]
-
 
 
  def after_sign_in_path_for(resource)
